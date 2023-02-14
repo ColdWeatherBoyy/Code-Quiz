b17476e7dd772572919c50de39b9a890032f1e70
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-02-13 23:11:57 -0500

    finishes javascript for quiz, begins set up of highscore page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// pseudo code
 //needed variables
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start-btn");
@@ -66,6 +65,7 @@ function setTime() {
     timerEl.textContent = "Timer: " + secondsLeft;
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
+      endGame();
     }
   }, 1000);
 }
@@ -79,117 +79,118 @@ function removeStarter() {
 }
 
 // creates ol list and answers with initial question and answers in proper styling
-  function createQuizContent() {  
-    quizTitle.setAttribute("style", "font-size: 1.5em; text-decoration: none;");
+function createQuizContent() {  
+  quizTitle.setAttribute("style", "font-size: 1.5em; text-decoration: none;");
+  quizTitle.textContent = questions[currentQuestion].question;
+  var answerList = document.createElement("ol");
+  answerList.setAttribute("style", "display: flex; flex-flow: column; align-items: center;")
+  quizArea.appendChild(answerList);
+  for (let x=0; x < questions[currentQuestion].answers.length; x++) {
+    var answersEl = document.createElement("button");
+    answersEl.setAttribute("class", "btn");
+    answersEl.setAttribute("style", "text-align: left; white-space: nowrap; width: fit-content; padding: 5px 10px")
+    answersEl.textContent = questions[currentQuestion].answers[x].text;
+    answerList.appendChild(answersEl);
+    answersEl.addEventListener("click", function() {answerValidation(x)});
+  }
+  var confirmation = document.createElement("p");
+  quizArea.appendChild(confirmation);
+  confirmation.setAttribute("style", "color: var(--text-dark); font-size: 1.5em;")
+  confirmation.classList.add('confirmation');
+}
+
+function answerValidation(x) {
+  var confirmation = document.querySelector(".confirmation");
+  var answersEls = document.querySelectorAll(".btn");
+  
+  if (questions[currentQuestion].answers[x].correct) {
+    confirmation.textContent = "That is correct!";
+  } else {
+    confirmation.textContent = "That is wrong!";
+    secondsLeft -= 10;
+  }
+  
+  currentQuestion++;
+  if (currentQuestion >= questions.length || secondsLeft <= 0) {
+    return endGame();
+  } else {
     quizTitle.textContent = questions[currentQuestion].question;
-    var answerList = document.createElement("ol");
-    answerList.setAttribute("style", "display: flex; flex-flow: column; align-items: center;")
-    quizArea.appendChild(answerList);
-    for (let x=0; x < questions[currentQuestion].answers.length; x++) {
-      var answersEl = document.createElement("button");
-      answersEl.setAttribute("class", "btn");
-      answersEl.setAttribute("style", "text-align: left; white-space: nowrap; width: fit-content; padding: 5px 10px")
-      answersEl.textContent = questions[currentQuestion].answers[x].text;
-      answerList.appendChild(answersEl);
-      answersEl.addEventListener("click", function() {answerValidation(x)});
+    for (let z=0; z < questions[currentQuestion].answers.length; z++) {
+      answersEls[z].textContent = questions[currentQuestion].answers[z].text;
     }
-    var confirmation = document.createElement("p");
-    quizArea.appendChild(confirmation);
-    confirmation.setAttribute("style", "color: var(--text-dark); font-size: 1.5em;")
-    confirmation.classList.add('confirmation');
+  }
+}
+
+function endGame() {
+  clearInterval(timerInterval);
+  timerEl.textContent = "Timer: " + secondsLeft;
+  var answersEls = document.querySelectorAll(".btn");
+  var confirmation = document.querySelector(".confirmation");
+
+  for (let z = 0; z < answersEls.length; z++) {
+    answersEls[z].remove();
+  }
+  confirmation.remove();
+
+  if (secondsLeft > 0) {
+    quizTitle.textContent = "Congratulations! You finished in time with a score of " +secondsLeft +". Please enter your initals below.";
+  } else {
+    quizTitle.textContent = "Better luck next time, but please still enter your initials!";
   }
 
-  function answerValidation(x) {
-    var confirmation = document.querySelector(".confirmation");
-    var answersEls = document.querySelectorAll(".btn");
-    
-    if (questions[currentQuestion].answers[x].correct) {
-      confirmation.textContent = "That is correct!";
+  var returnedValues = makeForm();
+  var submitBtn = returnedValues[0];
+  var initialSubmit = returnedValues[1];
+  submitBtn.addEventListener("click", function(event) {
+    event.preventDefault()
+    var userScore = {
+      user: initialSubmit.value,
+      score: secondsLeft 
+    }
+    if (JSON.parse(localStorage.getItem("storedScores")) === null) {
+      let userScores = [userScore];
+      localStorage.setItem("storedScores", JSON.stringify(userScores)); 
     } else {
-      confirmation.textContent = "That is wrong!";
-      secondsLeft -= 10;
+      let userScores = JSON.parse(localStorage.getItem("storedScores"));
+      userScores.push(userScore);
+      localStorage.setItem("storedScores", JSON.stringify(userScores));
     }
-    
-    currentQuestion++;
-    if (currentQuestion >= questions.length || secondsLeft <= 0) {
-      return endGame();
-    } else {
-      quizTitle.textContent = questions[currentQuestion].question;
-      for (let z=0; z < questions[currentQuestion].answers.length; z++) {
-        answersEls[z].textContent = questions[currentQuestion].answers[z].text;
-      }
-    }
-  };
+  });
+  submitBtn.addEventListener("click", function() {
+    document.location.href = "./index-score.html";
+  });
+}
 
-  function endGame() {
-    clearInterval(timerInterval);
-    var answersEls = document.querySelectorAll(".btn");
-    var confirmation = document.querySelector(".confirmation");
+function makeForm() {
+  var initialForm = document.createElement("form");
+  initialForm.setAttribute("style", "display: flex; flex-flow: column; align-content: center;")
+  quizArea.appendChild(initialForm);
+  var initialLabel = document.createElement("label");
+  initialLabel.setAttribute("for", "initial");
+  quizArea.appendChild(initialLabel);
+  var initialSubmit = document.createElement("input");
+  initialSubmit.setAttribute("type", "text");
+  initialSubmit.setAttribute("placeholder", "initials please!");
+  initialForm.appendChild(initialSubmit);
+  var submitBtn = document.createElement("button");
+  submitBtn.setAttribute("class", "btn");
+  submitBtn.textContent = "Submit now!"
+  initialForm.appendChild(submitBtn);
+  return [submitBtn, initialSubmit];
+}
 
-    for (let z = 0; z < answersEls.length; z++) {
-      answersEls[z].remove();
-    }
-    confirmation.remove();
-
-    if (secondsLeft > 0) {
-      quizTitle.textContent = "Congratulations! You finished in time with a score of " +secondsLeft +". Please enter your initals below.";
-    }
-
-    var returnedValues = makeForm();
-    var submitBtn = returnedValues[0];
-    var initialSubmit = returnedValues[1];
-    submitBtn.addEventListener("click", function(event) {
-      event.preventDefault()
-      var userScore = {
-        user: initialSubmit.value,
-        score: secondsLeft 
-      }
-      if (JSON.parse(localStorage.getItem("storedScores")) === null) {
-        let userScores = [userScore];
-        localStorage.setItem("storedScores", JSON.stringify(userScores)); 
-      } else {
-        let userScores = JSON.parse(localStorage.getItem("storedScores"));
-        userScores.push(userScore);
-        localStorage.setItem("storedScores", JSON.stringify(userScores));
-      }
-    });
+window.addEventListener("load", function() {
+  if(window.location.href.endsWith("/index-score.html")) {
+    displayScores();
   }
+});
 
-  function makeForm() {
-    var initialForm = document.createElement("form");
-    initialForm.setAttribute("style", "display: flex; flex-flow: column; align-content: center;")
-    quizArea.appendChild(initialForm);
-    var initialLabel = document.createElement("label");
-    initialLabel.setAttribute("for", "initial");
-    quizArea.appendChild(initialLabel);
-    var initialSubmit = document.createElement("input");
-    initialSubmit.setAttribute("type", "text");
-    initialSubmit.setAttribute("placeholder", "initials please!");
-    initialForm.appendChild(initialSubmit);
-    var submitBtn = document.createElement("button");
-    submitBtn.setAttribute("class", "btn");
-    submitBtn.textContent = "Submit now!"
-    initialForm.appendChild(submitBtn);
-    return [submitBtn, initialSubmit];
+function displayScores() {
+  var userScores = JSON.parse(localStorage.getItem("storedScores"));
+  var highscoreList = document.querySelector("#highscore-list");
+  for (let x = 0; x < userScores.length; x++)  {
+    var highscoreEls = document.createElement("li");
+    highscoreEls.textContent = userScores[x].user + ": " + userScores[x].score;
+    highscoreList.appendChild(highscoreEls);
   }
-  // checking correct or false mechanism needs to add to current question
-
- 
-  
-
-    // NEED TO FIX REPLACING ANSWER TEXT. SOLUTION HAS TO DO WITH HOW FOR LOOP WORKS AND HOW TO JUST REPLACE TEXT CONTENT. ELSE SHOWS ORIGINAL ATTEMPT BY RECALLING FUNCTION, BUT BETTER SOLUTION MAY BE FINDING A NEW WAY TO CYCLE THROUGH BUTTONS AND REPLACE TEXT CONTENT
-
-  
-
-// answering question triggers right or wrong
-// answering question wrong triggers timer -10
-// both answers trigger next question
-// on timer complete, score is given as time left – if time out, score is 0
-
-// store questions and answers as an object array with properties as answers
-
-// on game complete
-// static page with submission to local storage of name
-//submit button stores name and also takes to Highscore
-
-//highscore is static, make buttons on click to clear local storage or go back to main page at start of game
+}
